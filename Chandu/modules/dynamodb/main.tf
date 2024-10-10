879fffe9a643b46aa405d8bc3e197389b254e1af
Chandra Sekhar <103116370+Iamchnd@users.noreply.github.com>
2024-10-10 17:33:48 +0530

    Update main.tf

--- Chandu/modules/dynamodb/main.tf
+++ Chandu/modules/dynamodb/main.tf
@@ -1,1 +1,16 @@
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = var.table_name
+  billing_mode = "PAY_PER_REQUEST"
 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  hash_key = "LockID"
+}
+
+variable "table_name" {
+  description = "The name of the DynamoDB table"
+  type        = string
+}
