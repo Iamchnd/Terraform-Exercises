9970242078b6d5e91ab0fc9e3cf6217d87bdc17c
Chandra Sekhar <103116370+Iamchnd@users.noreply.github.com>
2024-10-10 17:34:30 +0530

    Update main.tf

--- Chandu/modules/ec2/main.tf
+++ Chandu/modules/ec2/main.tf
@@ -1,1 +1,24 @@
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+variable "ami_id" {
+  description = "The AMI ID for the instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of the instance"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "The name tag for the instance"
+  type        = string
+}
 
