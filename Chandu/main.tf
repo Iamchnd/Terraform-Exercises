80ceb7ce5b0bce04ab7071e5906d5f8b644c5149
Chandra Sekhar <103116370+Iamchnd@users.noreply.github.com>
2024-10-10 17:15:19 +0530

    Update main.tf

--- Chandu/main.tf
+++ Chandu/main.tf
@@ -1,1 +1,20 @@
+provider "aws" {
+  region = var.region
+}
 
+module "s3_bucket" {
+  source      = "./modules/s3"
+  bucket_name = "chandra-sekhar-bucket-vam-test" 
+}
+
+module "dynamodb_table" {
+  source    = "./modules/dynamodb"
+  table_name = "statefile-locking"
+}
+
+module "ec2_instance" {
+  source        = "./modules/ec2"
+  ami_id       = "ami-0dee22c13ea7a9a67" 
+  instance_type = "t2.micro"
+  instance_name = "ExampleInstance"
+}
