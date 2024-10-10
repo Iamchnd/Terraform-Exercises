943bd3bfd2e0b311821683ff99f8f3489af3ba70
Chandra Sekhar <103116370+Iamchnd@users.noreply.github.com>
2024-10-10 11:59:54 +0000

    Added variables

--- Chandu/variables.tf
+++ Chandu/variables.tf
@@ -2,3 +2,13 @@ variable "region" {
   description = "The AWS region to deploy resources"
   default     = "ap-south-1"  
 }
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+  default     = "chandra-sekhar-bucket-vam-test"
+}
+variable "ami_id"  {
+  description = "ami id"
+  type        = string
+  default     = "ami-0dee22c13ea7a9a67"
+}    
