c1cd96b41f3d9b81ab8e81dc7a60cc8b90c0acbb
Chandra Sekhar <103116370+Iamchnd@users.noreply.github.com>
2024-10-10 17:06:57 +0530

    Update backend.tf

--- Chandu/backend.tf
+++ Chandu/backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "chandra-sekhar-bucket-vam-test" 
+    key            = "terraform/state"
+    region         = var.region
+    dynamodb_table = "statefile-locking" 
+    encrypt        = true
+  }
+}
