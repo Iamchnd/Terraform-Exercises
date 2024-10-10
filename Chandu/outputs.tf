17897033c0a61640420d4a99783e99ca99b01337
Chandra Sekhar <103116370+Iamchnd@users.noreply.github.com>
2024-10-10 17:21:20 +0530

    Update outputs.tf

--- Chandu/outputs.tf
+++ Chandu/outputs.tf
@@ -1,1 +1,11 @@
+output "s3_bucket_id" {
+  value = module.s3_bucket.bucket_name
+}
 
+output "dynamodb_table_name" {
+  value = module.dynamodb_table.table_name
+}
+
+output "ec2_instance_id" {
+  value = module.ec2_instance.example.id
+}
